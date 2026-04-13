e3bf3c177de8e2e676355bf0deac66d208fc1e12
kursadbilgin <kursad.bilgin.7@gmail.com>
2026-04-13 13:14:35 +0300

    initialize spring boot project with docker postgresql setup

---
+++ src/main/java/com/kursad/linkpress/LinkpressApplication.java
@@ -0,0 +1,13 @@
+package com.kursad.linkpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kursad/linkpress/LinkpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursad.linkpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
